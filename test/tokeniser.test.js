5a3da607742335047afc02832bcde4418ae8a8c0
Michael Williamson <mike@zwobble.org>
2012-08-23 21:37:55 +0100

    Rename tag to keyTag

--- test/tokeniser.test.js
+++ test/tokeniser.test.js
@@ -1,12 +1,12 @@
 var tokeniser = require("../lib/tokeniser");
 
-exports["Tags are wrapped in curly braces"] = function(test) {
+exports["Key tags are wrapped in curly braces"] = function(test) {
     var tokens = tokeniser.tokenise("Mad {thing} in a {transport}...");
     test.deepEqual([
         tokeniser.literal("Mad "),
-        tokeniser.tag("thing"),
+        tokeniser.keyTag("thing"),
         tokeniser.literal(" in a "),
-        tokeniser.tag("transport"),
+        tokeniser.keyTag("transport"),
         tokeniser.literal("...")
     ], tokens);
     test.done();
