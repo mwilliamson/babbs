1ed1e852a0e35c5fafcb1e5dd0fa93a0d456e766
Michael Williamson <mike@zwobble.org>
2012-09-01 17:33:14 +0100

    Split out in-memory fs implementation to separate module

--- test/runLogging.test.js
+++ test/runLogging.test.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 var _ = require("underscore");
 var q = require("q");
+var fsInMemory = require("fs-in-memory");
 
 var runLogging = require("../lib/runLogging.js");
 
@@ -74,104 +75,5 @@ exports["next run result logs result to run number one if no previous run number
 };
 
 function createFs(files) {
-    function navigateTo(filePath) {
-        return pathToParts(filePath).then(function(parts) {
-            return navigateToInner(files, parts);
-        });
-    }
-    
-    function navigateToInner(current, filePathParts) {
-        if (filePathParts.length === 0) {
-            return q.resolve(current);
-        } else if (_.isString(current)) {
-            return q.reject(new Error("No such file"));
-        } else {
-            var next = current[filePathParts[0]];
-            if (next) {
-                return navigateToInner(next, filePathParts.slice(1));
-            } else {
-                return q.reject(new Error("No such file"));
-            }
-        }
-    }
-    
-    function isFile(current) {
-        return current && _.isString(current);
-    }
-    
-    function isDirectory(current) {
-        return current && !_.isString(current);
-    }
-    
-    function pathToParts(filePath) {
-        if (!/^\//.test(filePath)) {
-            return q.reject(new Error("Path must be absolute"));
-        }
-        var normalFilePath = path.normalize(filePath);
-        return q.resolve(normalFilePath.substring(1).split("/"));
-    }
-    
-    function readFile(filePath) {
-        return navigateTo(filePath).then(function(file) {
-            if (isFile(file)) {
-                return q.resolve(file);
-            } else {
-                return q.reject(filePath + " is not a file");
-            }
-        });
-    }
-    
-    function readdir(dirPath) {
-        return navigateTo(dirPath).then(function(file) {
-            if (isFile(file)) {
-                return q.reject(new Error(dirPath + " is not directory"));
-            } else {
-                return q.resolve(_.keys(file));
-            }
-        });
-    }
-    
-    function exists(filePath) {
-        return navigateTo(filePath).then(constant(true)).fail(constant(false));
-    }
-    
-    function mkdirp(filePath) {
-        return pathToParts(filePath).then(function(parts) {
-            var current = files;
-            for (var i = 0; i < parts.length; i += 1) {
-                if (isDirectory(current)) {
-                    current = current[parts[i]] = current[parts[i]] || {};
-                } else {
-                    return q.reject(new Error("/" + parts.slice(0, i).join("/") + " is not directory"));
-                }
-            }
-            return q.resolve();
-        });
-    }
-    
-    function writeFile(filePath, contents) {
-        var filename = path.basename(filePath);
-        return navigateTo(path.dirname(filePath)).then(function(parent) {
-            if (isDirectory(parent[filename])) {
-                return q.reject(new Error(filePath + " is directory"));
-            } else {
-                parent[filename] = contents;
-                return q.resolve();
-            }
-        });
-    }
-    
-    return {
-        readFile: readFile,
-        readdir: readdir,
-        exists: exists,
-        mkdirp: mkdirp,
-        writeFile: writeFile
-    };
-}
-
-function constant(value) {
-    return function() {
-        return value;
-    };
+    return fsInMemory.createPromised(files);
 }
